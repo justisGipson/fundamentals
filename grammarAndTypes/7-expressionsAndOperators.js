a3d5cbb2143a83d0c67621e01a4fedbf7f37bf93
Justis Gipson <justisgipson@Audra-Sloans-MacBook-Pro.local>
2018-12-04 15:26:48 -0500

    blah

---
+++ grammarAndTypes/7-expressionsAndOperators.js
@@ -0,0 +1,22 @@
+// Assignment Operators
+let x = 5;
+
+// Addition assignment
+x += 1; // x = x + 1;
+
+// Subtraction assignment
+x -= 1; // x = x - 1;
+
+// Multiplication assignment
+x *= 1; // x = x * 1;
+
+// Division assignment
+x /= 1; // x = x / 1;
+
+//Modulus assignment
+x %= 1; // x = x % 1;
+
+//Exponential assignment
+x **= 1; // x = x ** 1;
+
+console.log(x);
