a294f7fe8f8e5f09bd4bc7b3a34c6bedc9b55095
Justis Gipson <justisgipson@Audra-Sloans-MacBook-Pro.local>
2018-12-04 15:16:57 -0500

    update

--- grammarAndTypes/6-literals.js
+++ grammarAndTypes/6-literals.js
@@ -2,3 +2,17 @@ let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 console.log(days)
 console.log(days.toString());
 console.log(days.length);
+
+let tired = true;
+
+let soup = {
+    a:'chicken noodle',
+    b:'tomato',
+    c:'beef and barley'
+};
+
+console.log(soup.b);
+
+let car = 'Honda';
+
+let dec = 10.2;
