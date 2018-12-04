00dd7b396f54f436a9bbaf433819f2286ce4686f
Justis Gipson <justisgipson@Audra-Sloans-MacBook-Pro.local>
2018-12-04 16:02:07 -0500

    if stuff

---
+++ 2-controlFlowAndError/1-if.js
@@ -0,0 +1,18 @@
+let isOn = true;
+if (isOn == true) {
+    console.log('the light is on')
+};
+
+if (isOn) {
+    console.log('the light is still on')
+};
+
+let isOff = false;
+if(isOff == false){
+    console.log("it's off")
+};
+
+let weather = 65;
+if(weather < 70 && weather > 60) {
+    console.log('wear a jacket')
+};
