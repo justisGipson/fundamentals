8d0497ac62e779ed1996772d76e392bb888fdd04
Justis Gipson <justisgipson@Audra-Sloans-MacBook-Pro.local>
2018-12-04 15:09:29 -0500

    update

--- grammarAndTypes/4-hoisting.js
+++ grammarAndTypes/4-hoisting.js
@@ -3,3 +3,11 @@ scissors = 'blue';
 
 console.log(scissors);
 var scissors;
+
+b();
+console.log(a)
+
+function b(){
+    console.log('this is all hoisted')
+}
+var a = 'this is not hoisted'
---
+++ grammarAndTypes/6-literals.js
@@ -0,0 +1,4 @@
+let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+console.log(days)
+console.log(days.toString());
+console.log(days.length);
