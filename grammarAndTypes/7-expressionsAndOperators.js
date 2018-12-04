862efee68234162eaf6e1b6e15f78d3c9e6a532d
Justis Gipson <justisgipson@Audra-Sloans-MacBook-Pro.local>
2018-12-04 15:42:00 -0500

    uhh

--- grammarAndTypes/7-expressionsAndOperators.js
+++ grammarAndTypes/7-expressionsAndOperators.js
@@ -20,3 +20,25 @@ x %= 1; // x = x % 1;
 x **= 1; // x = x ** 1;
 
 console.log(x);
+
+/************************************************* */
+
+// Comparison operators
+console.log('3' == 3); //equal operator
+
+console.log('3' === 3); // strict equal operator
+
+console.log('3' != 3); // not equal
+
+console.log('3' !== 3); // strict not equal
+
+console.log(3 > 2); // greater than
+
+console.log(3 < 2); // less than
+
+console.log(3 >= 2); // greater than or equal to
+
+console.log(3 <= 2); // less than or equal to
+
+console.log(2 || 3); // OR operator
+/************************************************* */
