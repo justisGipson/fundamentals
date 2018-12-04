57864a1d970b0952d01d5db0a4741b77dc4d37c3
Justis Gipson <justisgipson@Audra-Sloans-MacBook-Pro.local>
2018-12-04 14:37:04 -0500

    updates

---
+++ grammarAndTypes/3-scope.js
@@ -0,0 +1,31 @@
+// var x = 12;
+
+// function scope() {
+//     x = 33;
+//     console.log(x);
+// }
+
+// scope();
+// console.log(x); 
+
+const x = 12;
+
+function scope() {
+    const x = 33
+    if (true) {
+        const x = 45;
+        console.log(x);
+    }
+    console.log(x);
+}
+
+scope();
+console.log(x);
+
+/*
+    variable grammer    obeys functional scope  obeys block scope   can be mutated (changed after initialization)
+    var                 x                       -                   x
+    let                 x                       x                   x
+    const               x                       x                   -
+*/
+
