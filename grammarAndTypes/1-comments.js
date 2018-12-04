bc97b45edd88e6dc159238a33d2b6d0e47d389c2
Justis Gipson <justisgipson@Audra-Sloans-MacBook-Pro.local>
2018-12-04 09:55:16 -0500

    first push

---
+++ grammarAndTypes/1-comments.js
@@ -0,0 +1,16 @@
+// will ignore anything after the backslash
+// what comments do for us:
+// 1) communicate with other devs by writing plain english
+// 2) annotate certain sections of our code for our future selves
+
+// grading structure...
+// 1) delivery 1-10 scale
+// 2) project -- portfolio
+// 3) project -- CSS creature
+// 4) project -- API
+// 5) quality and effort
+// 6) professionalism (dress nicer...)
+// 7) repl (5)
+// 8) whiteboard assesment
+// 9) web tutorial page
+//  make sure we can explain exactly what our code does...
---
+++ grammarAndTypes/2-declarations.js
@@ -0,0 +1,45 @@
+console.log("Hello World!");
+
+// cmd ? highlighted : comment out
+
+/* multi
+line comment */
+
+// console.log is a great tool to debug later down the line.
+
+var a = 1;
+var A = 2;
+
+console.log(A); // capitalization matters
+
+var b = 1;
+var b = 2;
+console.log(b); // 2
+
+let c = 3;
+c = 4;
+console.log(c);
+
+var firstName = 'Justis';
+const lastName = 'Gipson';
+console.log(firstName, lastName);
+
+/* 
+declarations are the left side of the variable
+    it is simply the var x
+    it is in the left side of the assignment operator
+Initializations are thr right side of the variable
+    it sets the value of the variable
+    it incorporates the variable name (x), the assignment operator (=), and the value (10) => x =10
+ */
+
+ var x
+ console.log('declaration', x);
+
+ x = 10
+ console.log('Initialization 1:', x);
+
+ var y = "Hello";
+ console.log("Both:", x, y);
+
+ 
